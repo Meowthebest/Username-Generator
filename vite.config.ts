4a87c36a25f1532057200b38cab9069b48161c9d
Cat <138353441+Meowthebest@users.noreply.github.com>
2025-12-23 17:35:58 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/USERNAME-Generator/', // <-- your repo name exactly
+  base: '/Username-Generator/', // <-- your repo name exactly
 })
